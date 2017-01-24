314f324235c297a54ca2a9fd4bc555953c04dbf0
AprioriC <andreas97fm@yandex.ru>
2017-01-24 19:21:44 +0300

    load
    load

---
+++ js/addPop.js
@@ -0,0 +1,22 @@
+/**
+ * Created by andre on 24.01.2017.
+ */
+"use strict";
+
+var preloader = document.getElementById("escapingBall_1");
+function fadeOutnojquery(el) {
+    el.style.opacity = 1;
+    var interhellopreloader = setInterval(function() {
+        el.style.opacity = el.style.opacity - 0.05;
+        if (el.style.opacity <= 0.05) {
+            clearInterval(interhellopreloader);
+            preloader.style.display = "none";
+        }
+    }, 10);
+};
+window.onload = function() {
+    setTimeout(function() {
+        fadeOutnojquery(preloader);
+    }, 1000);
+}
+;
